6c18d7c70e6cb392ae74735c1ca5af96a07bc9df
stevin0596 <17896440596@163.com>
2020-07-17 10:30:14 +0800

    update MainActivity.kt

--- app/src/main/java/com/zhiyi/demo/MainActivity.kt
+++ app/src/main/java/com/zhiyi/demo/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //this is a test edit from 0596
         //edit from 1021 local
+        //edit form 0596 at 10:29
     }
 }
